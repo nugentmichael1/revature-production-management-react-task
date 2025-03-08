cea5f2273f4df249d2b010fc9b63fed841d0838e
Michael Nugent <nugentmichael@live.com>
2025-03-07 19:55:22 -0800

    Add and comment out practice code.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,45 @@
 
 //Components
+// import { JSX, useState } from 'react';
 import TaskManagersComparison from './Components/TaskManagersComparison'
 //CSS
 import './CSS/App.css'
 
+// function Counter() {
+//   const [count, setCount] = useState(0);
+//   return <button onClick={() => setCount(count + 1)}>Click Me</button>;
+// }
+
+// function ItemList({ items }: { items: string[] }): JSX.Element {
+  // const itemsList = items.map((item) => (
+  //     <li>
+  //         {item}
+  //     </li>
+  // ));
+  // return <ul>{itemsList}</ul>
+// 	return <ul>{
+// 		items.map((item)=>(
+// 			<li>
+// 				{item}
+// 			</li>
+// 		))
+// 	}</ul>
+// }
+
 function App() {
+
+  const name = "React";
+  const element = <h1>Hello, {name}!</h1>;
+  console.log(element);
 
   return (
     <div className="App">
       <TaskManagersComparison />
+      {/* <Counter /> */}
+      {/* <ItemList items={["Item1", "Item2", "Item3"]} /> */}
     </div>
   )
 }
 
 export default App
+
